55263b82763c8db6dd9d8a50d464f61e39fe405f
songpengfei159 <62453456+songpengfei159@users.noreply.github.com>
2025-11-28 09:13:07 +0800

    init: 测试

--- src/main.rs
+++ src/main.rs
@@ -28,7 +28,6 @@ struct DeviceData {
 struct Params {
     battery: String,
     phone: String,
-    last_time: String,
 }
 
 
@@ -41,7 +40,7 @@ async fn query_and_send_to_redis() -> redis::RedisResult<()> {
     println!("connect mysql success");
     // 执行 SQL 查询
 
-    let results: Vec<(i32, i32, String, String)> = conn.query("SELECT CardID, UseStatus, phonenu,TimeStamps FROM MedBoxDevice where CardId = 28021833").unwrap();
+    let results: Vec<(i32, i32, String)> = conn.query("SELECT CardID, UseStatus, phonenu,TimeStamps FROM MedBoxDevice where CardId > 20000000").unwrap();
     println!("device size: {:?}", results.len());
     // 连接 Redis
     let client = Client::open("redis://39.106.149.139/").unwrap();
@@ -49,7 +48,7 @@ async fn query_and_send_to_redis() -> redis::RedisResult<()> {
     // 发送到 Redis Stream
     let stream_name = "iot_device_message"; // 可以根据需要修改 stream 名称
 
-    for (CardID, battery, phonenu,TimeStamps) in results {
+    for (CardID, battery, phonenu) in results {
         // 组装数据
         let request_id = Uuid::new_v4().to_string();
         // 获取当前时间戳（毫秒）
@@ -68,7 +67,6 @@ async fn query_and_send_to_redis() -> redis::RedisResult<()> {
             params: Params {
                 battery: battery.to_string(),
                 phone: phonenu,
-                last_time: TimeStamps,
             },
             data: None,
             code: None,
